2bda80d864053148685e945ada86041d1a94a051
LucasGl19 <148262082+LucasGl19@users.noreply.github.com>
2025-11-02 22:28:40 +0100

    R03: Implementado el acceso a la página del perfil de usuario [JIRA-UH03]

--- undersounds-frontend/src/app/app.routes.ts
+++ undersounds-frontend/src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { SongsComponent } from './pages/songs/songs.component';
 import { LayoutBasicoComponent } from './layouts/layout-basico/layout-basico.component';
 import { EmptyComponent } from './pages/empty/empty.component';
 import { ArtistsComponent } from './pages/artists/artists.component';
-
+import { ProfileComponent } from './pages/profile/profile.component';
 export const routes: Routes = [
   {
     path: '',
@@ -11,6 +11,7 @@ export const routes: Routes = [
     children: [
       { path: 'songs', component: SongsComponent },
       { path: 'artists', component: ArtistsComponent },
+      { path: 'profile', component: ProfileComponent },
       { path: 'home', component: EmptyComponent }
     ]
   }
