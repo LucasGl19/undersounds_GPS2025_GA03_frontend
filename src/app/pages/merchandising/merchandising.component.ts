e6afa00c4d1c5046224c5aa88ca18c9cb562b86f
LucasGl19 <148262082+LucasGl19@users.noreply.github.com>
2025-11-11 20:25:43 +0100

    GPS-154 Añadir campo de búsqueda en frontend

--- src/app/pages/merchandising/merchandising.component.ts
+++ src/app/pages/merchandising/merchandising.component.ts
@@ -3,37 +3,59 @@ type MerchItem = {
   price: string;
   description: string;
   image: string;
+  createdAt: string;
 };
 
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-merchandising',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './merchandising.component.html',
   styleUrl: './merchandising.component.css',
 })
-export class MerchandisingComponent {
+export class MerchandisingComponent implements OnInit{
+  searchQuery: string = '';
+  filteredArticles: MerchItem[] = [];
   readonly articles: MerchItem[] = [
     {
       name: 'Camiseta “Waves”',
       price: '22 €',
       description: 'Algodón orgánico con impresión serigráfica de edición limitada.',
       image: 'assets/images/merch/merch-shirt.svg',
+      createdAt: '01/03/2023',
     },
     {
       name: 'Taza esmaltada',
       price: '12 €',
       description: 'Perfecta para sesiones nocturnas de mezcla o para tu café matutino.',
       image: 'assets/images/merch/merch-mug.svg',
+      createdAt: '15/07/2023',
     },
     {
       name: 'Tote bag “UnderSounds”',
       price: '15 €',
       description: 'Bolsa resistente para llevar vinilos, cassettes y equipos ligeros.',
       image: 'assets/images/merch/merch-tote.svg',
+      createdAt: '22/11/2022',
     },
   ];
+
+    ngOnInit(): void {
+      this.filteredArticles = this.articles;
+    }
+
+    searchArticles(): void {
+    const query = this.searchQuery.toLowerCase().trim();
+    if(!query) {
+      this.filteredArticles = this.articles;
+      return;
+    }
+     this.filteredArticles = this.articles.filter(article =>
+      article.name.toLowerCase().includes(query) 
+    );
+  }
 }
