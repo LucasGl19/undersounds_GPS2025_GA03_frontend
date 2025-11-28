1255c9d29bbb46b682cf5cbd9cbfe44ba2b9a7d1
EnriqueSCJ <enriquetec2004@gmail.com>
2025-11-28 19:54:52 +0100

    Fix: Cabecera minimo privilegio

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,11 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, APP_INITIALIZER, provideZoneChangeDetection } from '@angular/core';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter, UrlSerializer } from '@angular/router';
 
 import { routes } from './app.routes';
 import { authInterceptor } from './interceptors/auth.interceptor';
 import { LowerCaseUrlSerializer } from './router/lowercase-url-serializer';
+import { AuthService } from './services/auth.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,5 +13,11 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor])),
     { provide: UrlSerializer, useClass: LowerCaseUrlSerializer },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: (auth: AuthService) => () => auth.initialize(),
+      deps: [AuthService],
+      multi: true,
+    },
   ]
 };
